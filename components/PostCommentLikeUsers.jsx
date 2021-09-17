7619feeee9e76ef3b68679b8cae6ff8bf23a2b19
shamu <kssoe99@gmail.com>
2021-09-17 18:22:22 +0630

    Processing with Like

--- components/PostCommentLikeUsers.jsx
+++ components/PostCommentLikeUsers.jsx
@@ -7,14 +7,21 @@ import { main } from '@popperjs/core'
 
 function PostCommentLikeUsers() {
     const [users,setUser]= useState([
-        {id:1,name:"Shamu",isLoggin:false},
-        {id:2,name:"Krishan",isLoggin:true}
+        {id:1,name:"Shamu",isLoggin:true},
+        {id:2,name:"Krishan",isLoggin:false}
     ])
+
+    
+    
     const [posts,setPost] = useState([
-        {id:1,title:"Django",description:"Web framework written in Python",likecount:0},
-        {id:2,title:"Laravel",description:"PHP Written Framework",likecount:0},
-        {id:3,title:"ROR",description:"Ruby on Rail Framework",likecount:0},
-        {id:4,title:"Spring",description:"Java Framework for user advance enhancement",likecount:0}
+        {id:1,title:"Django",description:"Web framework written in Python",likecount:1,
+        likeby:[{id:1,name:"Shamu",isLoggin:true}],
+        comments:[
+            {user:{id:1,name:"Shamu",isLoggin:true},commentText:"This is for  Django Comment"}
+        ]},
+        {id:2,title:"Laravel",description:"PHP Written Framework",likecount:0,likeby:[]},
+        {id:3,title:"ROR",description:"Ruby on Rail Framework",likecount:0,likeby:[]},
+        {id:4,title:"Spring",description:"Java Framework for user advance enhancement",likecount:1,likeby:[ {id:2,name:"Krishan",isLoggin:true}]}
     
     ])
     const[viewProduct,setviewProduct] = useState([])
@@ -33,11 +40,16 @@ function PostCommentLikeUsers() {
         setcanView(!canView)
     }
 
-    const  handleLikeCount = (post) => {
-        
+    const  handleLikeCount = (post,user) => {
+        console.log("user",user,"Main")
+       
         let finalData = posts.map(function(p){
             if(p.id === post.id){
+                
+                const getUser = user[0]
+               
                 p.likecount ++
+                p.likeby=[...p.likeby,getUser]
                 return post
             }
             return p
@@ -61,7 +73,8 @@ function PostCommentLikeUsers() {
                 <ViewForm post={viewProduct} 
                         isView={canView} 
                         onLikeCount={handleLikeCount} 
-                        onClose={handleClose}  />
+                        onClose={handleClose}
+                        user={users.filter(u => u.isLoggin === true)}  />
             </div>
            
         </div>
@@ -73,7 +86,7 @@ export default PostCommentLikeUsers
 
 const Post = (props) => {
     const [isView,setisView] = useState(false)
-    const [post,setPost] = useState({id:props.post.id,title:props.post.title,description:props.post.description,likecount:props.post.likecount})
+    const [post,setPost] = useState({id:props.post.id,title:props.post.title,description:props.post.description,likecount:props.post.likecount,likeby:props.post.likeby})
     const handleViewForm = (post) => {
         setisView(!isView)
         // console.log("On POST",post)
@@ -113,10 +126,9 @@ const ViewForm = (props) => {
        props.onClose()
     // console.log("Closing")
     }
-    const handleLike = (post) => {
-       
-
-        props.onLikeCount(post)
+    const handleLike = (post,user,e) => {
+        e.preventDefault()
+        props.onLikeCount(post,user)
        
         // setLikecount(likecount =>likecount +1)
     }
@@ -128,22 +140,44 @@ const ViewForm = (props) => {
         console.log("Shared")
     }
     if(props.isView){
+        const apk = props.post.likeby.filter(u => u.id === props.user[0].id).length
+        console.log(apk)
+           if(props.post.likeby.length >=1 ){
+               
+                const dataMap = props.post.likeby.map(u => u.id === props.user[0].id)
+                const dataFilter = props.post.likeby.filter(u => u.id === props.user[0].id).length
+                // console.log(dataFilter)
+                // console.log(dataMap)
+            //    props.user[0] in props.post.likeby
+           }
+     
         return (
-   
+
 
                
                 <div>
-                     <p>{JSON.stringify(props.post)}</p>
+                     {/* <p>{JSON.stringify(props.post)}</p> */}
                     <div className="close">
                     <p onClick={handleClose} ><FontAwesomeIcon icon={faWindowClose} /></p>
                     </div>
                     <form>
-                            <h1>View Form</h1>
                             
+                            <p>Give Like Comment and Share for the post as u  like</p>
                             <h3>{props.post.title}</h3>
                             <p>{props.post.description}</p>
                             <div className="like-comment-share">
-                                <span  onClick={()=>handleLike(props.post)}> Like {props.post.likecount}<FontAwesomeIcon icon={faHeart} /></span>
+                                {/* //checking condition */}
+                               { props.post.likeby.filter(user => user.id === props.user[0].id).length >0 ? (
+                                   <button disabled  className="some-class"  
+                                   onClick={(e)=>handleLike(props.post,props.user,e)}> Like {props.post.likecount} 
+                                   <span className="text text-danger"><FontAwesomeIcon icon={faHeart} /></span>
+                                   </button>
+                                ): 
+                                    <button   className="some-class"  
+                                    onClick={(e)=>handleLike(props.post,props.user,e)}> Like {props.post.likecount} 
+                                    <span className="lovereact"><FontAwesomeIcon icon={faHeart} /></span></button>
+                                }
+                               
                                 <span  onClick={handleComment}>Comment<FontAwesomeIcon icon={faComment} /></span>
                                 <span onClick={handleShare}>Share<FontAwesomeIcon icon={faShare} /></span>
                             </div>
